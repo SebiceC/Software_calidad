0eaddcca6415ebbabf0a885d7dae73743ce5d9e1
SebiceC <sebartin9@gmail.com>
2024-11-23 21:55:48 -0500

    adicion de las demas paginas y cambios de formato

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,64 +1,41 @@
-/**
-=========================================================
-* Material Kit 2 React - v2.1.0
-=========================================================
+import './styles/App.css';
+import RegistroUsuario from './components/RegistroUsuario';
+import Login from './components/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import EmpresaForm from './components/EmpresaForm';
+import SeleccionarActividad from './components/SeleccionarActividad';
+import Evaluacion from './components/Evaluacion';
+import Resultados from './components/Resultados';
+import MatrizRiesgos from './components/MatrizRiesgos';
+import MatrizMitigacionRiesgos from './components/MatrizMitigacionRiesgos';
+import EliminarDatos from './components/EliminarDatos';
+import UserHome from './pages/UserHome';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-* Product Page: https://www.creative-tim.com/product/material-kit-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-import { useEffect } from "react";
-
-// react-router components
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
-
-// @mui material components
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-
-// Material Kit 2 React themes
-import theme from "assets/theme";
-import Presentation from "layouts/pages/presentation";
-
-// Material Kit 2 React routes
-import routes from "routes";
-
-export default function App() {
-  const { pathname } = useLocation();
-
-  // Setting page scroll to 0 when changing the route
-  useEffect(() => {
-    document.documentElement.scrollTop = 0;
-    document.scrollingElement.scrollTop = 0;
-  }, [pathname]);
-
-  const getRoutes = (allRoutes) =>
-    allRoutes.map((route) => {
-      if (route.collapse) {
-        return getRoutes(route.collapse);
-      }
-
-      if (route.route) {
-        return <Route exact path={route.route} element={route.component} key={route.key} />;
-      }
-
-      return null;
-    });
-
+function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    <Router>
+      <div>
+        <Navbar />
+      </div>
       <Routes>
-        {getRoutes(routes)}
-        <Route path="/presentation" element={<Presentation />} />
-        <Route path="*" element={<Navigate to="/presentation" />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<RegistroUsuario />} />
+        <Route path="/empresa" element={<EmpresaForm />} />
+        <Route path="/actividad" element={<SeleccionarActividad />} />
+        <Route path="/evaluacion" element={<Evaluacion />} />
+        <Route path="/resultados" element={<Resultados />} />
+        <Route path="/matriz-riesgos" element={<MatrizRiesgos />} />
+        <Route path="/matriz-mitigacion" element={<MatrizMitigacionRiesgos />} />
+        <Route path="/eliminar" element={<EliminarDatos />} />
+        <Route path='/UserHome' element={<UserHome />} />
       </Routes>
-    </ThemeProvider>
+      <div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
+
+export default App;
